5a8e034830cbfd9454a89f01c0be206dc035e997
Narius2030 <buiducnhan15122003@gmail.com>
2024-04-29 15:51:03 +0700

    feat: modify the paths

--- airflow/plugins/clean_data.py
+++ airflow/plugins/clean_data.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 from data_scrape.dtprocess import cleandt
 from pyvi import ViTokenizer
 
+ROOT_PATH = '/mnt/d/Programming/Vietnamese-Text-Generator/'
+
 def lower_case(x):
     try:
         x = x.lower()
@@ -19,7 +21,7 @@ def transform():
     """Transform the raw data to usable text
     """
     ## Import data from raw folder to dataframe
-    CRAWL_FOLDER = '/mnt/d/Programming/Vietnamese-Text-Generator/data/test/raw'
+    CRAWL_FOLDER = f'{ROOT_PATH}/data/test/raw'
     vnexpress = []
 
     for filename in os.listdir(CRAWL_FOLDER):
@@ -53,16 +55,16 @@ def transform():
 
     ## Tokenize the Vietnamese words
     processed_news['tag'] = processed_news['tag'].apply(lambda x: ViTokenizer.tokenize(x))
-    processed_news['tag'] = processed_news['tag'].apply(lambda x: cleandt.remove_stopword(x, './src/vietnamese-stopwords.txt'))
+    processed_news['tag'] = processed_news['tag'].apply(lambda x: cleandt.remove_stopword(x, f'{ROOT_PATH}/data/vietnamese-stopwords.txt'))
     
     return raw_news, processed_news
 
 def dump(raw_news, processed_news):
     """Dump each tag to a text file
     """
-    raw_news.to_csv('/mnt/d/Programming/Vietnamese-Text-Generator/data/test/csv/vnexpress.csv')
+    raw_news.to_csv(f'{ROOT_PATH}/data/test/csv/vnexpress.csv')
     
-    PROCESSED_FOLDER = '/mnt/d/Programming/Vietnamese-Text-Generator/data/test/processed'
+    PROCESSED_FOLDER = f'{ROOT_PATH}/data/test/processed'
     for topic in tqdm(os.listdir(PROCESSED_FOLDER)):
         articles_ids, tags = get_info(topic, processed_news)
         # print(articles_ids)
